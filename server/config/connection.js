e22fee069d0b5824a6a8a08e2cdeb8949200c978
Brandon_Lopez <brandonchristopherlopez@gmail.com>
2023-12-11 19:20:00 -0800

    adjusting .env

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 //require('dotenv').config(); // remove on production
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI);// || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
